f2313a481db8aaeea87e2d866a8ae40c81d89945
Will G <sniper7kills@users.noreply.github.com>
2021-01-23 09:56:29 -0500

    Update ManuallyIdentified.php
    Directly pass the $tenant regardless if it is valid or not.
    Add a check to see if it is a valid tenant class and set variable accordingly

--- src/Events/ManuallyIdentified.php
+++ src/Events/ManuallyIdentified.php
@@ -4,7 +4,21 @@ namespace ArlonWIP\Tenancy\Recorder\Events;
 
 use Tenancy\Tenant\Events\Event;
 
-class ManuallyIdentified extends Event
+class ManuallyIdentified
 {
-    
+    /**
+     * @var Tenant
+     */
+    public $tenant;
+
+    public $valid_tenant_class = false;
+
+    public function __construct($tenant)
+    {
+        $this->tenant = $tenant;
+
+        if ($this->tenant instanceof \Tenancy\Identification\Contracts\Tenant) {
+            $this->valid_tenant_class = true;
+        }
+    }
 }
