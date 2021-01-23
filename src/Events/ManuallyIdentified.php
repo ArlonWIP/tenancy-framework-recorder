468036aeb993d62c3d0e5c66f56911e27ebd7bb9
Will G <sniper7kills@users.noreply.github.com>
2021-01-23 14:00:07 -0500

    Update ManuallyIdentified.php

--- src/Events/ManuallyIdentified.php
+++ src/Events/ManuallyIdentified.php
@@ -6,19 +6,16 @@ use Tenancy\Tenant\Events\Event;
 
 class ManuallyIdentified
 {
-    /**
-     * @var Tenant
-     */
     public $tenant;
 
-    public $valid_tenant_class = false;
+    public $validTenantClass = false;
 
     public function __construct($tenant)
     {
         $this->tenant = $tenant;
 
         if ($this->tenant instanceof \Tenancy\Identification\Contracts\Tenant) {
-            $this->valid_tenant_class = true;
+            $this->validTenantClass = true;
         }
     }
 }
